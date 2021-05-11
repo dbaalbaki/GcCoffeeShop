4951864df1e3c904c4f699df3da929ca38a88eea
dbaalbaki <80474797+dbaalbaki@users.noreply.github.com>
2021-05-11 15:50:39 -0400

    Coffee Website

---
+++ CoffeeShopReg/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using CoffeeShopReg.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopReg.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddUser(string firstname, string lastname, string password, string email, string phonenumber)
+        {
+            ViewData["FirstName"] = firstname;
+            ViewData["LastName"] = lastname;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopReg/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoffeeShopReg.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
